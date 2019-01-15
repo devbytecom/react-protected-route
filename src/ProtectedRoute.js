e016ad13a9d8434f42991b20734744140410b885
DONEBROS\shaun.brown <shaun.brown@betfred.com>
2019-01-15 11:05:59 +0000

    Fix for prop types

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { bool, string, func } from 'prop-types';
+import PropTypes from 'prop-types';
 
 class ProtectedRoute extends Component {
     static propTypes = {
-        isAuthenticated: bool.isRequired,
-        redirectTo: string.isRequired,
-        component: func
+        isAuthenticated: PropTypes.bool,
+        redirectTo: PropTypes.string,
+        component: PropTypes.oneOfType([PropTypes.string, PropTypes.func])
     };
 
     static defaultProps = {
@@ -17,7 +17,7 @@ class ProtectedRoute extends Component {
     render() {
         const { isAuthenticated, redirectTo, component: ProtectedComponent, ...rest } = this.props;
 
-        return <Route render={props => (isAuthenticated ? <ProtectedComponent {...props} /> : <Redirect to={redirectTo} />)} {...rest} />;
+        return <Route {...rest} render={props => (isAuthenticated ? <ProtectedComponent {...props} /> : <Redirect to={redirectTo} />)} />;
     }
 }
 
